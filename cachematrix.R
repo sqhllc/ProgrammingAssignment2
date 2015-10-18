5d767363ea0935035a72fd110469798c6f9db88b
sqhllc <sqhllc@gmail.com>
2015-10-18 16:52:39 -0400

    improve comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,26 @@
 cachematrix <- function(data) { 
-##This function creates a special "matrix" object and computes the inverse solution. If
+## This function creates a special "matrix" object and computes the inverse solution. If
 ## an inverse solution doesn't already exists, function will create inverse solution.
 ## Otherwise, function will retrieve solution from cache memory.
-##This function assumes that all matrix supplied will be invertible.
-## For example, an invertible matris is  data <-diag(x=10,5,5)
+## This function assumes that all matrix supplied will be invertible.
+## For example, an invertible matrix is  data <-diag(x=10,5,5)
+
 ## The function makeCacheMatrix creates a special matrix object that can cache its inverse
   
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL #inverse
+  m <- NULL # initialize inverse matrix variable
+
+  ## Initialize cache
   set <- function(y) {
     x <<- y
     m <<- NULL
   }
-  get <-function () x
+  ## Define functions for accessing the cache
+  get <- function () x
   setinverse <- function(inv) m <<- inv
   getinverse <- function() m
   
+  ## Packaging accessor functions for the cache
   list (set=set, get=get, setinverse = setinverse, getinverse = getinverse)
   }
 
@@ -23,21 +28,23 @@ makeCacheMatrix <- function(x = matrix()) {
 ## from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
   if (!is.null(m)) { 
     message ("getting cache data")
     return(m)
   }
-  data <- x$get()
-  m <- solve(data)
-  x$setinverse(m)
+  data <- x$get()  # retrieve the original matrix
+  m <- solve(data) # compute the inverse of the original matrix
+  x$setinverse(m)  # store the inverse in cache
   m
 }
 
+## Demonstration of caching of matrix inverse
 cm <- makeCacheMatrix(data)
-a<-cacheSolve(cm)
+a<-cacheSolve(cm) 
 print(a)
-b<-cacheSolve(cm)
+b<-cacheSolve(cm) 
 print(b)
 }
